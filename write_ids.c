98e993b6a72f7c0d7cf2c94a0b3552c2d4f0a498
tahasarfrazs <tsb1901@gmail.com>
2024-09-16 22:39:23 -0400

    Your message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "tahasarfraz";
+    char STUDENT_ID[] = "999572793";
 
     return 0 ;
 }
